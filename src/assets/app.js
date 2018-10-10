e575739328ac0e265d1c32888e3b0ebd4d37ade4
PaulaAraya <paula.arayawilliams@gmail.com>
2018-10-09 22:45:22 -0300

    borra todos los post

--- src/assets/app.js
+++ src/assets/app.js
@@ -5,16 +5,21 @@ let arrayPost = [];
 
 const saveComment = (post) => { 
   arrayPost.push(post);
+  postPrint.innerHTML = '';
 }
 
 const deleteComment = (i) => {
   arrayPost.splice(i,1);
+// Eliminamos el hijo (childpost) del elemento padre
+for ( i ; i < postPrint.children.length; i++) {
+  let postChild = postPrint.children[i]
+  postPrint.remove(postChild )
+}
 };
 
 const printComment = (posts) => {
   postPrint.innerHTML = '';
   posts.forEach((post, index) => {
-    
     postPrint.innerHTML += `
     <p>${post}</p> <button onclick="deleteComment(${index})"> eliminar </button>
     `;
@@ -24,8 +29,9 @@ const printComment = (posts) => {
 
 btn.addEventListener('click',()=> {
       let savePost = textArea.value;
+      textArea.value = '';
       if (savePost === ''){
-        alert("porfavor escriba un texto")
+        alert("por favor escriba un texto")
       }else{
       saveComment(savePost);
       printComment(arrayPost);
