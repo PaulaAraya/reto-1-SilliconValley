8fd43bfc4d80b80487ef17a4af8959bd60d094c2
PaulaAraya <paula.arayawilliams@gmail.com>
2018-10-09 23:30:37 -0300

    post se elminan uno a uno correctamente

--- src/assets/app.js
+++ src/assets/app.js
@@ -3,18 +3,13 @@ const btn = document.getElementById('btn');
 const postPrint = document.getElementById('printPost');
 let arrayPost = [];
 
-const saveComment = (post) => { 
+const saveComment = (post) => {
   arrayPost.push(post);
-  postPrint.innerHTML = '';
 }
 
 const deleteComment = (i) => {
-  arrayPost.splice(i,1);
-// Eliminamos el hijo (childpost) del elemento padre
-for ( i ; i < postPrint.children.length; i++) {
-  let postChild = postPrint.children[i]
-  postPrint.remove(postChild )
-}
+  arrayPost.splice(i, 1);
+  printComment(arrayPost);
 };
 
 const printComment = (posts) => {
@@ -23,18 +18,16 @@ const printComment = (posts) => {
     postPrint.innerHTML += `
     <p>${post}</p> <button onclick="deleteComment(${index})"> eliminar </button>
     `;
-    console.log(index);
   })
 };
 
-btn.addEventListener('click',()=> {
-      let savePost = textArea.value;
-      textArea.value = '';
-      if (savePost === ''){
-        alert("por favor escriba un texto")
-      }else{
-      saveComment(savePost);
-      printComment(arrayPost);
-      console.log(arrayPost);
-    }
-    });
+btn.addEventListener('click', () => {
+  let savePost = textArea.value;
+  textArea.value = '';
+  if (savePost === '') {
+    alert("por favor escriba un texto")
+  } else {
+    saveComment(savePost);
+    printComment(arrayPost);
+  }
+});
